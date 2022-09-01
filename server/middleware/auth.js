de5278f23add0d5889fa96237d2dab12fa6ef8f6
SharkWen <10713718+myshark@user.noreply.gitee.com>
2022-09-01 23:24:20 +0800

    修改上一次提交

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,8 +5,10 @@ module.exports = options => {
   return async (req, res, next) => {
     const token = String(req.headers.authorization || '').split(' ').pop();
     assert(token, 401, "请先登录1");
+    let id;
     jwt.verify(token, req.app.get('secret'), (err, decoded) => {
       if (err) throw new Error("token过时或解析错误请重新登录");
+      id = decoded.id;
     });
     req.user = await AdminUser.findById(id)
     assert(req.user, 401, "请先登录3");
